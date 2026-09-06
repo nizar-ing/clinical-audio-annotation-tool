f90b9b36d15603e5e4cd6e72764766f444c4ae34
Nizar ING <nizar.ilahi@gmail.com>
2026-09-06 15:13:57 +0100

    chore: configure typescript monorepo

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.yarn/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+);
